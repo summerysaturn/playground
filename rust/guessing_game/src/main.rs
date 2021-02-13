0c6acff7cd83e2ac03406123b1cedca884531a70
Char <48696244+summerysaturn@users.noreply.github.com>
2021-02-13 04:24:38 +0000

    chore(rust): :wrench: tidy up guessing game akin to rustbook

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -7,13 +7,9 @@ fn main() {
 
 	let secret_number = rand::thread_rng().gen_range(1,101);
 
-	println!("The secret number is: {}", secret_number);
-
-	println!("Please input your guess.");
-
-
-
   loop {
+
+    println!("Please input your guess.");
 
     let mut guess = String::new();
 
@@ -21,17 +17,21 @@ fn main() {
       .read_line(&mut guess)
       .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    let guess: u32 = guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
 
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
       Ordering::Less => println!("Too small!"),
       Ordering::Greater => println!("Too big!"),
-      Ordering::Equal => break,
+      Ordering::Equal => {
+        println!("You win!");
+        break;
+      },
     }
 
   }
-
-  println!("You win!")
 }
